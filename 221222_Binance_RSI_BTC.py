744ba01fdf1218f9e80213ba1bdd27c595bdc07b
worldest0628@gmail.com <worldest0628@gmail.com>
2023-02-09 20:45:28 +0900

    //투입 가격 변경 //현재 가격 표시 //수익률은 entryPrice  어떻게 받는 지 모르겠음

--- 221222_Binance_RSI_BTC.py
+++ 221222_Binance_RSI_BTC.py
@@ -7,7 +7,6 @@ import json
 import time
 import platform
 import line_alert #라인 메세지를 보내기 위함!
-
 from binance import Client
 
 from datetime import datetime
@@ -23,8 +22,8 @@ RSI_criteria_2 = 19
 
 profit_rate = 2
 
-RSI_criteria_1_GetInMoney = 700
-RSI_criteria_2_GetInMoney = 1500
+RSI_criteria_1_GetInMoney = 400
+RSI_criteria_2_GetInMoney = 800
 
 #암복호화 클래스 객체를 미리 생성한 키를 받아 생성한다.
 simpleEnDecrypt = myBinance.SimpleEnDecrypt(ende_key.ende_key)
@@ -89,7 +88,9 @@ for i, open_order in enumerate(order):
 current_time = datetime.now(timezone('Asia/Seoul'))
 KR_time=str(current_time)
 KR_time_sliced =KR_time[:23]
-RSI_string = "  ♥♥" +KR_time_sliced+"♥♥  \n"+'[NOW 1hour RSI_바이낸스] : ' +str(round(rsi_hour,2))+"\n"
+RSI_string = "  ♥♥" +KR_time_sliced+"♥♥  \n"+'[NOW 1hour RSI_바이낸스] : ' +str(round(rsi_hour,2))\
+             +"\n"+ '[NOW 가격] : ' + str(round(now_price_binance,2))+" $" 
+
 
 for j,profit_rate_i in enumerate(profit_rate_list):
     RSI_string += '[' + str(j+1) + ". 수익%] : " + str(profit_rate_i) + " 투입 $ : " + str(round(invested_money[j],1)) +"\n"
